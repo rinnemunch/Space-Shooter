dbe6524d54ec4f37191d18cede43d85158c09912
Shaun Fulton <fulton_shaun@yahoo.com>
2025-03-10 07:26:07 -0400

    Laser Position Offset

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,7 +24,7 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
         }
     }
 
