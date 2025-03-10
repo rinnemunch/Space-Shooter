4786008b25355e2d797bb9ef7d9499c26df77b37
Shaun Fulton <fulton_shaun@yahoo.com>
2025-03-10 07:11:19 -0400

    Instantiate Laser

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,8 +6,10 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab; 
 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,12 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
     }
 
     void CalculateMovement()
