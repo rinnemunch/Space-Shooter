a01f349b16381bde7ee1889fd4e56282063dd93e
Shaun Fulton <fulton_shaun@yahoo.com>
2025-03-09 20:45:30 -0400

    Speed Variable

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    private float _speed = 3.5f; 
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,6 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * 5 *  Time.deltaTime);
+        transform.Translate(Vector3.right * _speed *  Time.deltaTime);
     }
 }
