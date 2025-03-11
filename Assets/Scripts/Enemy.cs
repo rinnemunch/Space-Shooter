3fe328dd4513e07f6f3f5e406b05d3aa77369391
Shaun Fulton <fulton_shaun@yahoo.com>
2025-03-11 10:09:22 -0400

    Destroy Enemy and trigger collisions

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -23,4 +23,18 @@ public class Enemy : MonoBehaviour
             transform.position = new Vector3(randomX, 7, 0);
         }
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(this.gameObject);  
+        }
+
+        if (other.tag == "Laser")
+        {
+            Destroy(this.gameObject);
+            Destroy(other.gameObject);
+        }
+    }
 }
