0b738ed8ebcb1bfc5ed10bee5391dbe99f27808f
Shaun Fulton <fulton_shaun@yahoo.com>
2025-03-11 10:38:55 -0400

    Player lives and damaging

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -28,6 +28,12 @@ public class Enemy : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            Player player = other.transform.GetComponent<Player>();
+
+            if (player != null)
+            {
+                player.Damage();
+            }
             Destroy(this.gameObject);  
         }
 
