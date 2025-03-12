6567b094b6b420923f5a273c326b452a494dac1a
Shaun Fulton <fulton_shaun@yahoo.com>
2025-03-12 13:50:18 -0400

    Tidy up the spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -6,6 +6,8 @@ public class SpawnManager : MonoBehaviour
 {
     [SerializeField]
     private GameObject _enemyPrefab;
+    [SerializeField]
+    private GameObject _enemyContainer; 
     // Start is called before the first frame update
     void Start()
     {
@@ -23,7 +25,8 @@ public class SpawnManager : MonoBehaviour
         while (true)
         {
             Vector3 posToSpawn = new Vector4(Random.Range(-8f, 8f), 7, 0);
-            Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            GameObject newEnemy = Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            newEnemy.transform.parent = _enemyContainer.transform;
             yield return new WaitForSeconds(5.0f);
         }
     }
