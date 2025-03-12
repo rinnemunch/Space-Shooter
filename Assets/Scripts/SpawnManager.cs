708a32f12e1509cf4a616abace6dadbc341b93bc
Shaun Fulton <fulton_shaun@yahoo.com>
2025-03-12 03:20:01 -0400

    Spawn Routine

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _enemyPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRountine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnRountine()
+    {
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector4(Random.Range(-8f, 8f), 7, 0);
+            Instantiate(_enemyPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+    }
+}
